c91a1b34c965dd154c0e4cf85e08487b8636d988
Alisaryn <powersmichelle904@gmail.com>
2025-09-26 12:45:41 -0400

    Update FScriptTE.pas

--- FScriptTE.pas
+++ FScriptTE.pas
@@ -1101,7 +1101,7 @@ end;
 procedure TfmScriptTE.About1Click(Sender: TObject);
 begin
   Application.MessageBox('    Script Text Editor and version 1.0-1.1c updates by Alisaryn.'
-  + #13#10 + '    Contact through Discord for any feature requests or issues!', 'About', 0);
+  + #13#10 + '    Contact through Discord for any feature requests or issues.', 'About', 0);
 end;
 
 procedure TfmScriptTE.AddArgs1Click(Sender: TObject);
