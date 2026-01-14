784756cf02d2515a6d4a9e6435683500a25bf54f
Alisaryn <85263592+Alisaryn@users.noreply.github.com>
2026-01-14 15:02:52 -0500

    Color adjustments for dark themes

--- main.pas
+++ main.pas
@@ -2479,7 +2479,7 @@ begin
         px2 := px2 / Zoom;
         py := py + mmy + MidP[Floor[sfloor].Obj[x].map_section].y + px2;
         if darkmode then
-          BBRelBmp.Canvas.Brush.Color := RGB(40, 110, 60)
+          BBRelBmp.Canvas.Brush.Color := RGB(45, 120, 65)
         else
           BBRelBmp.Canvas.Brush.Color := ClGreen;
         BBRelBmp.Canvas.FillRect(Rect(round(px) - round(6 / Zoom), round(py) - round(6 / Zoom),
